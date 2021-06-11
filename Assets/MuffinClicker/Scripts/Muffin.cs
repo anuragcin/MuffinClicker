0fbb70bb798737d5d02418a5f412c2af159c7203
Anurag Chawla <chawla.anurag@hotmail.com>
2021-06-11 09:32:29 -0700

    Added Challenges..
    Added Challenges..

--- Assets/MuffinClicker/Scripts/Muffin.cs
+++ Assets/MuffinClicker/Scripts/Muffin.cs
@@ -9,6 +9,9 @@ public class Muffin : MonoBehaviour
     public TMP_Text muffinText;
     public RectTransform[] spinLight;
     public float[] spinLightSpeed;
+    public int noOfMuffins;
+    public float qualityAward;
+    public int noOfClicks;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,8 @@ public class Muffin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      
+
         for(int indx = 0; indx < spinLight.Length; indx++)
         {
             spinLight[indx].Rotate(0, 0, spinLightSpeed[indx] * Time.deltaTime);
@@ -28,12 +33,32 @@ public class Muffin : MonoBehaviour
 
     private void UpdateMuffin()
     {
-        muffinText.text = totalMuffins + " muffins";
+        //Extra Challenge
+        var txtMuffin = string.Empty;
+
+        if (totalMuffins == 1)
+        {
+            txtMuffin = "muffin";
+        }
+        else
+        {
+            txtMuffin = "muffins";
+        }
+
+        if (noOfClicks > qualityAward)
+        {
+            qualityAward = Time.time + 0.01f;
+
+            totalMuffins += 10 * noOfMuffins;
+        }
+
+        muffinText.text = totalMuffins + txtMuffin;
     }
 
     public void OnMuffinClicked()
     {
-        totalMuffins += 1;
+        noOfClicks++;
+        totalMuffins += noOfMuffins;
         UpdateMuffin();
     }
 }
