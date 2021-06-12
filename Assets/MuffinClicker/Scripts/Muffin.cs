d5aadf81a158b952a7e5bb42933a721a4907bed5
Anurag Chawla <chawla.anurag@hotmail.com>
2021-06-11 19:37:55 -0700

    Updated Challenges..
    Updated Challenges..

--- Assets/MuffinClicker/Scripts/Muffin.cs
+++ Assets/MuffinClicker/Scripts/Muffin.cs
@@ -10,13 +10,12 @@ public class Muffin : MonoBehaviour
     public RectTransform[] spinLight;
     public float[] spinLightSpeed;
     public int noOfMuffins;
-    public float qualityAward;
     public int noOfClicks;
-
+ 
     // Start is called before the first frame update
     void Start()
     {
-        UpdateMuffin();
+        UpdateUI();
     }
 
     // Update is called once per frame
@@ -31,7 +30,10 @@ public class Muffin : MonoBehaviour
         
     }
 
-    private void UpdateMuffin()
+    /// <summary>
+    /// Update UI
+    /// </summary>
+    private void UpdateUI()
     {
         //Extra Challenge
         var txtMuffin = string.Empty;
@@ -45,20 +47,31 @@ public class Muffin : MonoBehaviour
             txtMuffin = "muffins";
         }
 
-        if (noOfClicks > qualityAward)
-        {
-            qualityAward = Time.time + 0.01f;
-
-            totalMuffins += 10 * noOfMuffins;
-        }
-
         muffinText.text = totalMuffins + txtMuffin;
     }
 
+    /// <summary>
+    /// Muffin Clicked Method
+    /// </summary>
     public void OnMuffinClicked()
     {
         noOfClicks++;
         totalMuffins += noOfMuffins;
-        UpdateMuffin();
+        UpdateUI();
+    }
+
+    /// <summary>
+    /// Critical Clicked Method
+    /// </summary>
+    public void OnCriticalClick()
+    {
+        //Extra Spicy Challenge
+        float OnePercentOfClicks = 1.0f / noOfClicks;
+        if (OnePercentOfClicks == 0.01f)
+        {
+            totalMuffins += 10 * noOfMuffins;
+            UpdateUI();
+        }
+        
     }
 }
