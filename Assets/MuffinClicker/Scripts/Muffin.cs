0a68d6188d3080bcb41c7f2f831d8c512bc0a9be
Anurag Chawla <chawla.anurag@hotmail.com>
2021-06-16 00:06:22 -0700

    Updated Code for Critical Click - Call CreateFloatingText("Critical Clicked");
    Updated Code for Critical Click - Call CreateFloatingText("Critical Clicked");

--- Assets/MuffinClicker/Scripts/Muffin.cs
+++ Assets/MuffinClicker/Scripts/Muffin.cs
@@ -12,10 +12,11 @@ public class Muffin : MonoBehaviour
     public RectTransform[] spinLight;
     public float[] spinLightSpeed;
     public int noOfMuffins;
-    public int noOfClicks;
     public RectTransform muffinButton;
-    
- 
+    public int muffinsPerClick = 1;
+    public GameObject floatingTextPrefab;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -59,7 +60,6 @@ public class Muffin : MonoBehaviour
     /// </summary>
     public void OnMuffinClicked()
     {
-        noOfClicks++;
         totalMuffins += noOfMuffins;
 
         muffinText.color = Color.yellow;
@@ -68,9 +68,37 @@ public class Muffin : MonoBehaviour
         //Grow the muffin slightly..
         muffinButton.DOPunchScale(Vector3.one * Random.Range(0.0f,0.5f), 0.5f);
 
-        
+        // Create a floating text displaying the number of muffins collected
+        CreateFloatingText("+" + muffinsPerClick);
+
         UpdateUI();
     }
+
+    private void CreateFloatingText(string message)
+    {
+        // Calculate a random position for the floating text
+        Vector2 randomPosition = GetRandomFloatingTextPosition();
+
+        // Spawn a new floating text
+        GameObject floatingText = Instantiate(floatingTextPrefab, transform);
+
+        // Position the new floating text at the random position
+        floatingText.transform.localPosition = randomPosition;
+
+        // Set the floating text's text to the passing in message
+        floatingText.GetComponent<TMP_Text>().text = message;
+    }
+
+    private Vector2 GetRandomFloatingTextPosition()
+    {
+        // Generate a random X and Y position
+        float x = Random.Range(-200f, 200f);
+        float y = Random.Range(75f, 225f);
+
+        // Create and return a new Vector2 using those X and Y positions
+        return new Vector2(x, y);
+    }
+
 
     /// <summary>
     /// Critical Clicked Method
@@ -79,11 +107,12 @@ public class Muffin : MonoBehaviour
     {
 
         //Extra Spicy Challenge
-        //float OnePercentOfClicks = 1.0f / noOfClicks;
-        float OnePercentOfClicks = (float)System.Math.Round(Random.value, 2);
-         if (OnePercentOfClicks == 0.01f)
+        float OnePercentOfClicks = Random.value;
+         if (OnePercentOfClicks >= 0.99) //1%
         {
             totalMuffins += 10 * noOfMuffins;
+            // Create a floating text displaying Critical Clicked
+            CreateFloatingText("Critical Clicked");
             UpdateUI();
         }
         
