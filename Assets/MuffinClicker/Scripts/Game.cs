cd866e201ce4c11687117d3fe41f9854a68b7582
Anurag Chawla <chawla.anurag@hotmail.com>
2021-06-23 14:58:49 -0700

    Updated with Load and Save Game
    Updated with Load and Save Game

--- Assets/MuffinClicker/Scripts/Game.cs
+++ Assets/MuffinClicker/Scripts/Game.cs
@@ -1,50 +1,60 @@
-using TMPro;
+using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class Game : MonoBehaviour
 {
-
     public int totalMuffins;
-    public int muffinsPerClick = 1;
     public AudioClip muffinClickSound;
+    public int muffinsPerClick = 1;
     public FloatingText floatingTextPrefab;
     public Transform floatingTextContainer;
     public float criticalClickChance = 0.01f;
     public int criticalClickMultiplier = 10;
 
-    private AudioSource audioSource;
     private HeaderUI headerUI;
-    
+    private AudioSource audioSource;
 
     private void Awake()
     {
         headerUI = FindObjectOfType<HeaderUI>();
         audioSource = GetComponent<AudioSource>();
+
+        // Load the game
+        LoadGame();
+    }
+
+    private void OnApplicationQuit()
+    {
+        // Save our game
+        SaveGame();
     }
 
     public void OnMuffinCollected()
     {
-        //Critical Clicks
-
         bool isCritical = false;
-        int numMuffinCollected = muffinsPerClick;
+        int numMuffinsCollected = muffinsPerClick;
 
-        if (Random.value < criticalClickChance)
+        // Handle critical clicks
+        if(Random.value < criticalClickChance)
         {
             isCritical = true;
-            numMuffinCollected = muffinsPerClick * criticalClickMultiplier;
-           
+            numMuffinsCollected = muffinsPerClick * criticalClickMultiplier;
         }
 
-        totalMuffins += numMuffinCollected;
+        // Award the collected muffins
+        totalMuffins += numMuffinsCollected;
 
+        // Create a floating text displaying the number of muffins collected
+        // TODO: Maybe this floating text creation can be refactored elsewhere?
+        CreateFloatingText("+" + numMuffinsCollected, isCritical);
 
-        CreateFloatingText("+" + numMuffinCollected, isCritical);
+        // Flash the muffins text
         headerUI.FlashMuffinText();
 
+        // Play the muffin click sound
         audioSource.PlayOneShot(muffinClickSound);
-     }
+    }
 
     private void CreateFloatingText(string message, bool isImportant)
     {
@@ -57,9 +67,8 @@ public class Game : MonoBehaviour
         // Position the new floating text at the random position
         floatingText.transform.localPosition = randomPosition;
 
-        floatingText.GetComponent<FloatingText>().SetMessage(message,isImportant);
-
-        
+        // Set the floating text's message
+        floatingText.SetMessage(message, isImportant);
     }
 
     private Vector2 GetRandomFloatingTextPosition()
@@ -72,6 +81,51 @@ public class Game : MonoBehaviour
         return new Vector2(x, y);
     }
 
-    
+    private void Update()
+    {
+        // If F12 is pressed
+        if(Input.GetKeyDown(KeyCode.F12))
+        {
+            ResetGame();
+        }
+    }
+
+    private void ResetGame()
+    {
+        totalMuffins = 0;
+    }
+
+    private void SaveGame()
+    {
+        // Create a SaveData object
+        SaveData saveData = new SaveData();
+
+        // Populate the save data with the current game's data
+        saveData.totalMuffins = totalMuffins;
+
+        // Convert that save data object into JSON
+        string saveDataJSON = JsonUtility.ToJson(saveData);
+        Debug.Log(saveDataJSON);
+
+        // Save the JSON string to PlayerPrefs (NOTE: this could be saved to anywhere however)
+        PlayerPrefs.SetString("savegame", saveDataJSON);
+    }
+
+    private void LoadGame()
+    {
+        // Get the JSON save data string from PlayerPrefs (NOTE: this could be loaded from anywhere however)
+        string saveDataJSON = PlayerPrefs.GetString("savegame", "{}");
+        Debug.Log(saveDataJSON);
+
+        // Deserialize the JSON into the save data object
+        SaveData saveData = JsonUtility.FromJson<SaveData>(saveDataJSON);
+
+        // Set the current game state to use the saved values
+        totalMuffins = saveData.totalMuffins;
+    }
 }
 
+public class SaveData
+{
+    public int totalMuffins;
+}
